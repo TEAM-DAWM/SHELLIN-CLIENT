82f6cb71f4a122079d1c2bf310afe215e910d497
JeeminYi <yjjmmm000@g.skku.edu>
2024-11-14 00:28:16 +0900

    init: theme font 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,13 @@
 import button from './buttonTheme';
 import color from './color';
+import font from './font';
 import fontTheme from './fontTheme';
 import palette from './palette';
 import textButton from './textButtonTheme';
 
 export const theme = {
 	color,
+	font,
 	textButton,
 	button,
 	palette,
