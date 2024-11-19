51096178daf4e3b825a97a00da417b9009d8c891
wrryu09 <rryu09@ewhain.net>
2024-11-19 22:22:05 +0900

    docs: 드롭다운버튼 스토리북 등록

---
+++ src/stories/Common/Button/DropdownButton.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import DropdownButton from '@/components/common/v2/DropdownButton';
+
+const meta = {
+	title: 'Common/Button/DropdownButton',
+	component: DropdownButton,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+} satisfies Meta<typeof DropdownButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Incomplete: Story = {
+	args: {
+		status: '미완료',
+	},
+};
+
+export const Inprogress: Story = {
+	args: {
+		status: '진행중',
+	},
+};
+export const Complete: Story = {
+	args: {
+		status: '완료',
+	},
+};
