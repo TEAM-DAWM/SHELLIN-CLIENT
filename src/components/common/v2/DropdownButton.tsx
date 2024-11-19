21fd753f93bce57c7c1c741c77fe2570b1ea96bb
wrryu09 <rryu09@ewhain.net>
2024-11-19 22:21:56 +0900

    feat: 드롭다운버튼 제작

---
+++ src/components/common/v2/DropdownButton.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+import Button from './Button';
+
+import Icons from '@/assets/svg/index';
+
+// 추후 type 로 빼기
+type TaskStatus = '미완료' | '진행중' | '완료';
+type DropdownButtonProps = {
+	status: TaskStatus;
+};
+
+function DropdownButton({ status }: DropdownButtonProps) {
+	/** status에 따른 버튼 스타일 타입 설정 */
+	const getDropdownBtnType = () => {
+		switch (status) {
+			case '미완료':
+				return 'outlined-assistive';
+			case '진행중':
+				return 'solid';
+			case '완료':
+				return 'outlined-primary';
+
+			default:
+				return 'outlined-assistive';
+		}
+	};
+
+	return (
+		<SizedDropDownButton
+			type={getDropdownBtnType()}
+			label={status}
+			size="medium"
+			disabled={false}
+			rightIcon={<Icons.Navbar.IcnNavDashboard />}
+		/>
+	);
+}
+
+// 적용안됨
+const SizedDropDownButton = styled(Button)`
+	width: 9.6rem;
+	${({ theme }) => theme.shadow.FloatingAction1};
+`;
+export default DropdownButton;
