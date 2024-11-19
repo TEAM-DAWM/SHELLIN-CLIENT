e141659eb870ce64112e603e0983f1acfcd9e153
wrryu09 <rryu09@ewhain.net>
2024-11-19 16:26:50 +0900

    docs: 토글 스위치버튼 스토리북 등록

---
+++ src/stories/Common/Button/ToggleSwitchButton.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import ToggleSwitchButton from '@/components/common/v2/ToggleSwitchButton';
+
+const meta = {
+	title: 'Common/ToggleSwitchButton',
+	component: ToggleSwitchButton,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	args: { onClick: fn() },
+} satisfies Meta<typeof ToggleSwitchButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ToggleSwitchOnButton: Story = {
+	args: {
+		active: true,
+		onClick: fn(),
+	},
+};
