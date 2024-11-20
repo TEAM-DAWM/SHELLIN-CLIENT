c8396de24cb44ba841fa747da14e960675c435f6
wrryu09 <rryu09@ewhain.net>
2024-11-20 18:13:06 +0900

    fix: css 추가 적용

--- src/components/common/v2/control/DropdownButton.tsx
+++ src/components/common/v2/control/DropdownButton.tsx
@@ -1,4 +1,5 @@
-import styled from '@emotion/styled';
+import { css, useTheme } from '@emotion/react';
+import { useState } from 'react';
 
 import Button from '../button/Button';
 
@@ -11,6 +12,15 @@ type DropdownButtonProps = {
 };
 
 function DropdownButton({ status }: DropdownButtonProps) {
+	const { shadow } = useTheme();
+
+	/** 임시 state */
+	// open 은 이 컴포넌트 내부에서 다루어질 state 같아서 우선 props로 받지 않았습니다.
+	// 필요하면 위치 변경 등 추가 작업 자유롭게 하세요
+	const [isOpen, setOpen] = useState(false);
+	const handleOpen = () => setOpen((prev) => !prev);
+	/** 임시 state */
+
 	/** status에 따른 버튼 스타일 타입 설정 */
 	const getDropdownBtnType = () => {
 		switch (status) {
@@ -26,20 +36,23 @@ function DropdownButton({ status }: DropdownButtonProps) {
 		}
 	};
 
+	const customStyle = css`
+		justify-content: space-between;
+		width: 9.6rem;
+		${isOpen && shadow.FloatingAction1}
+	`;
+
 	return (
-		<SizedDropDownButton
+		<Button
 			type={getDropdownBtnType()}
 			label={status}
 			size="medium"
 			disabled={false}
-			rightIcon={<Icons.Navbar.IcnNavDashboard />}
+			rightIcon={isOpen ? <Icons.Navbar.IcnNavDashboard /> : <Icons.Navbar.IcnNavSetting />}
+			additionalCss={customStyle}
+			onClick={handleOpen}
 		/>
 	);
 }
 
-// 적용안됨
-const SizedDropDownButton = styled(Button)`
-	width: 9.6rem;
-	${({ theme }) => theme.shadow.FloatingAction1};
-`;
 export default DropdownButton;
