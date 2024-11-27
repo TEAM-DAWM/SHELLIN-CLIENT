7290e9bf5cbce96d0aecd33e25df3fecd919a230
Kjiw0n <rwd0904@naver.com>
2024-11-28 00:18:35 +0900

    style: add color token

---
+++ src/styles/colorToken.ts
@@ -0,0 +1,51 @@
+import color from './color';
+
+const colorToken = {
+	Primary: {
+		normal: color.Blue.Blue6,
+		strong: color.Blue.Blue7,
+		heavy: color.Blue.Blue8,
+		strongVariant: color.Blue.Blue2,
+		heavyVariant: color.Blue.Blue3,
+	},
+	Neutral: {
+		normal: color.Grey.White,
+		strong: color.Grey.Grey2,
+		heavy: color.Grey.Grey3,
+		accent: color.Grey.Grey4,
+	},
+	Text: {
+		primary: color.Blue.Blue7,
+		primaryVariant: color.Blue.Blue3,
+		neutralDark: color.Grey.White,
+		neutralLight: color.Grey.Grey8,
+		assistive: color.Grey.Grey6,
+		disable: color.Grey.Grey4,
+	},
+	Outline: {
+		primaryNormal: color.Blue.Blue4,
+		primaryStrong: color.Blue.Blue7,
+		neutralNormal: color.Grey.Grey3,
+		neutralStrong: color.Grey.Grey4,
+	},
+	Divider: {
+		neutral: color.Grey.Grey4,
+		primary: color.Blue.Blue3,
+	},
+	Component: {
+		normal: color.Grey.White,
+		strong: color.Blue.Blue2,
+		heavy: color.Blue.Blue3,
+		accent: color.Blue.Blue4,
+		assistive: color.Blue.Blue1,
+	},
+	Icon: {
+		normal: color.Grey.Grey5,
+		strong: color.Grey.Grey7,
+		heavy: color.Grey.Grey8,
+		primary: color.Blue.Blue7,
+		inverse: color.Grey.White,
+	},
+};
+
+export default colorToken;
