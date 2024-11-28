e8d1c003df5f23b6bf1d34ef2e87c14263565a88
wrryu09 <rryu09@ewhain.net>
2024-11-28 11:01:43 +0900

    fix: CheckButton 아이콘명으로 받도록 수정

--- src/components/common/v2/control/CheckButton.tsx
+++ src/components/common/v2/control/CheckButton.tsx
@@ -1,4 +1,3 @@
-import Icon from '../../Icon';
 import Button from '../button/Button';
 
 type CheckButtonProps = {
@@ -9,13 +8,12 @@ type CheckButtonProps = {
 };
 
 function CheckButton({ label, size = 'small', checked = false, onClick }: CheckButtonProps) {
-	const iconSize = size === 'small' ? 'tiny' : 'medium';
 	const iconType = checked ? 'IcnCheck' : 'IcnSquare';
 	return (
 		<Button
 			type={checked ? 'text-primary' : 'text-assistive'}
 			size={size}
-			leftIcon={<Icon name={iconType} size={iconSize} />}
+			leftIcon={iconType}
 			disabled={false}
 			label={label}
 			onClick={onClick}
