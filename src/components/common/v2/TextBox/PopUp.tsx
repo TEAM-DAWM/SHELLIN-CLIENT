6504c52977ccb542a9dba65860ccb6d1b6ddba98
Kjiw0n <rwd0904@naver.com>
2024-12-10 18:57:17 +0900

    refactor: useInputHandler 통해 코드 간결화

--- src/components/common/v2/TextBox/PopUp.tsx
+++ src/components/common/v2/TextBox/PopUp.tsx
@@ -1,18 +1,13 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
 
 import Icon from '../../Icon';
+
+import useInputHandler from '@/hooks/useInputHandler';
+import { INPUT_STATE, InputState } from '@/types/inputStateType';
 
 const TYPE = {
 	TITLE: 'title',
 	DESC: 'description',
-} as const;
-
-const STATE = {
-	PLACEHOLDER: 'placeholder',
-	DEFAULT: 'default',
-	TYPING: 'typing',
-	FIELD: 'field',
 } as const;
 
 type PopUpProps = {
@@ -20,31 +15,11 @@ type PopUpProps = {
 };
 
 function PopUp({ type }: PopUpProps) {
-	const [state, setState] = useState<(typeof STATE)[keyof typeof STATE]>(STATE.DEFAULT);
-
-	const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-		if (!e.target.value) {
-			setState(STATE.PLACEHOLDER);
-		}
-	};
-
-	const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-		if (e.target.value) {
-			setState(STATE.FIELD);
-		} else {
-			setState(STATE.DEFAULT);
-		}
-	};
-
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		if (e.target.value) {
-			setState(STATE.TYPING);
-		}
-	};
+	const { state, handleFocus, handleBlur, handleChange } = useInputHandler();
 
 	return (
 		<PopUpContainer>
-			{type === TYPE.DESC && state === STATE.DEFAULT && <Icon name="IcnDescription" size="tiny" />}
+			{type === TYPE.DESC && state === INPUT_STATE.DEFAULT && <Icon name="IcnDescription" size="tiny" />}
 			<StyledInput
 				type={type}
 				state={state}
@@ -66,18 +41,18 @@ const PopUpContainer = styled.div`
 	align-items: center;
 `;
 
-const StyledInput = styled.input<{ type: PopUpProps['type']; state: (typeof STATE)[keyof typeof STATE] }>`
+const StyledInput = styled.input<{ type: PopUpProps['type']; state: InputState }>`
 	${({ type, theme }) => (type === TYPE.TITLE ? theme.font.title02 : theme.font.body03)};
 	caret-color: ${({ theme }) => theme.color.Blue.Blue7};
 
 	color: ${({ state, theme }) => {
 		switch (state) {
-			case STATE.DEFAULT:
+			case INPUT_STATE.DEFAULT:
 				return theme.color.Grey.Grey6;
-			case STATE.PLACEHOLDER:
+			case INPUT_STATE.PLACEHOLDER:
 				return theme.color.Grey.Grey4;
-			case STATE.TYPING:
-			case STATE.FIELD:
+			case INPUT_STATE.TYPING:
+			case INPUT_STATE.FIELD:
 				return theme.color.Grey.Grey8;
 			default:
 				return theme.color.Grey.Grey6;
@@ -88,6 +63,6 @@ const StyledInput = styled.input<{ type: PopUpProps['type']; state: (typeof STAT
 	border-width: 0;
 
 	&::placeholder {
-		color: ${({ state, theme }) => (state === STATE.DEFAULT ? theme.color.Grey.Grey6 : theme.color.Grey.Grey4)};
+		color: ${({ state, theme }) => (state === INPUT_STATE.DEFAULT ? theme.color.Grey.Grey6 : theme.color.Grey.Grey4)};
 	}
 `;
