353e43ac8bbe0383ba3bdc37f78bb3ee55a098df
moonseonghui <msh9980@naver.com>
2024-12-12 18:36:08 +0900

    feat: Icn 컴포넌트 매개변수에 color추가

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,3 +1,4 @@
+import { Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import Icn from '@/assets/svg/V2';
@@ -5,12 +6,13 @@ import Icn from '@/assets/svg/V2';
 type IconProps = {
 	name: keyof typeof Icn;
 	size?: 'tiny' | 'small' | 'medium' | 'large' | 'xlarge';
+	color?: 'nomal' | 'strong' | 'heavy' | 'primary' | 'inverse';
 };
 
-function Icon({ name, size = 'medium' }: IconProps) {
+function Icon({ name, size = 'medium', color }: IconProps) {
 	const SelectedIcon = Icn[name];
 	return (
-		<StyledIconWrapper size={size}>
+		<StyledIconWrapper size={size} color={color}>
 			<SelectedIcon />
 		</StyledIconWrapper>
 	);
@@ -25,7 +27,19 @@ const sizeMap = {
 	large: '3.2rem',
 	xlarge: '4rem',
 };
-const StyledIconWrapper = styled.div<{ size: 'tiny' | 'small' | 'medium' | 'large' | 'xlarge' }>`
+
+const getColorMap = (theme: Theme) => ({
+	nomal: theme.colorToken.Icon.normal,
+	strong: theme.colorToken.Icon.strong,
+	heavy: theme.colorToken.Icon.heavy,
+	primary: theme.colorToken.Icon.heavy,
+	inverse: theme.colorToken.Icon.inverse,
+});
+
+const StyledIconWrapper = styled.div<{
+	size: 'tiny' | 'small' | 'medium' | 'large' | 'xlarge';
+	color?: 'nomal' | 'strong' | 'heavy' | 'primary' | 'inverse';
+}>`
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -35,5 +49,7 @@ const StyledIconWrapper = styled.div<{ size: 'tiny' | 'small' | 'medium' | 'larg
 	svg {
 		width: 100%;
 		height: 100%;
+
+		${({ theme, color }) => color && `color: ${getColorMap(theme)[color]};`}
 	}
 `;
