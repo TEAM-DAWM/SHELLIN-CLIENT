ebce6c94fde1cd5c5ed73da88059c8ef1f3e5ae0
wrryu09 <rryu09@ewhain.net>
2024-12-25 23:10:42 +0900

    fix: 사이드바 위치 변경

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,12 +1,9 @@
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 
-import NavBar from '@/components/common/NavBar';
-
 function MainLayout() {
 	return (
 		<MainLayOutContainer>
-			<NavBar />
 			<Outlet />
 		</MainLayOutContainer>
 	);
