d751c99932acb92fdf77a860e26f56222593f23c
wrryu09 <rryu09@ewhain.net>
2025-01-13 00:34:37 +0900

    feat: orderTask 연결

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 import useGetTasks from '@/apis/tasks/getTask/query';
+import useOrderTask from '@/apis/tasks/orderTask/query';
 import useUpdateTaskStatus from '@/apis/tasks/updateTaskStatus/query';
 import BtnTaskContainer from '@/components/common/BtnTaskContainer';
 import FullCalendarBox from '@/components/common/fullCalendar/FullCalendarBox';
@@ -27,6 +28,7 @@ function Today() {
 	const { data: stagingData } = useGetTasks({ sortOrder });
 	const { data: targetData, isError: isTargetError } = useGetTasks({ sortOrder, targetDate });
 	const { mutate, queryClient } = useUpdateTaskStatus(null);
+	const { mutate: orderTasksMutate } = useOrderTask();
 	const handleSidebar = () => {
 		setDumpAreaOpen((prev) => !prev);
 	};
@@ -71,39 +73,70 @@ function Today() {
 		// 드래그가 끝난 위치가 없으면 리턴
 		if (!destination) return;
 
-		// sourceTasks와 destinationTasks를 배열로 변환
-		const sourceTasks = source.droppableId === 'target' ? [...targetData] : [...stagingData];
-		const destinationTasks = destination.droppableId === 'target' ? [...targetData] : [...stagingData];
+		const updatedTargetData: TaskType[] = [...targetData];
+		const updatedStagingData: TaskType[] = [...stagingData];
+		let movedTask: TaskType;
 
-		// 드래그된 항목을 sourceTasks에서 제거하고 destinationTasks에 추가
-		const [movedTask] = sourceTasks.splice(source.index, 1);
-		destinationTasks.splice(destination.index, 0, movedTask);
-
-		// 상태 업데이트
 		if (source.droppableId === 'target') {
-			queryClient.setQueryData(['tasks'], {
-				target: { ...targetData, data: { ...targetData, tasks: sourceTasks } },
-				staging: { ...stagingData, data: { ...stagingData, tasks: destinationTasks } },
-			});
+			[movedTask] = updatedTargetData.splice(source.index, 1);
+			if (destination.droppableId === 'target') {
+				updatedTargetData.splice(destination.index, 0, movedTask);
+			} else {
+				updatedStagingData.splice(destination.index, 0, movedTask);
+			}
 		} else {
-			queryClient.setQueryData(['tasks'], {
-				target: { ...targetData, data: { ...targetData, tasks: destinationTasks } },
-				staging: { ...stagingData, data: { ...stagingData, tasks: sourceTasks } },
-			});
+			[movedTask] = updatedStagingData.splice(source.index, 1);
+			if (destination.droppableId === 'staging') {
+				updatedStagingData.splice(destination.index, 0, movedTask);
+			} else {
+				updatedTargetData.splice(destination.index, 0, movedTask);
+			}
 		}
 
+		queryClient.setQueryData(['tasks'], {
+			target: { ...targetData, tasks: updatedTargetData },
+			staging: { ...stagingData, tasks: updatedStagingData },
+		});
+
 		// API 호출
-		if (destination.droppableId === 'target') {
+		// staging -> target area: 해당 날짜로 설정, 미완료 상태 지정
+		if (destination.droppableId === 'target' && source.droppableId === 'staging') {
 			mutate({
 				taskId: movedTask.id,
 				targetDate,
 				status: '미완료',
 			});
-		} else if (destination.droppableId === 'staging') {
+			// target -> staging area: 상태 초기화
+		} else if (destination.droppableId === 'staging' && source.droppableId === 'target') {
 			mutate({
 				taskId: movedTask.id,
 				targetDate: null,
 				status: null,
+			});
+		}
+		// staging -> staging: custom order post api 호출
+		else if (
+			destination.droppableId === 'staging' &&
+			source.droppableId === 'staging' &&
+			sortOrder === 'CUSTOM_ORDER'
+		) {
+			const newOrder = updatedStagingData.map((item) => item.id);
+			orderTasksMutate({
+				type: true,
+				taskList: newOrder,
+			});
+
+			// target -> target: custom order post api 호출
+		} else if (
+			destination.droppableId === 'target' &&
+			source.droppableId === 'target' &&
+			sortOrder === 'CUSTOM_ORDER'
+		) {
+			const newOrder = updatedTargetData.map((item) => item.id);
+			orderTasksMutate({
+				type: false,
+				targetDate,
+				taskList: newOrder,
 			});
 		}
 	};
