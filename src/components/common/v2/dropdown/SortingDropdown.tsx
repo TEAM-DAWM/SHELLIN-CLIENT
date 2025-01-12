a872122b2783f58d86cf878820cb1ada8bb45049
wrryu09 <rryu09@ewhain.net>
2025-01-12 22:15:06 +0900

    fix: SortingDropdown 타입 수정, 스타일 수정

--- src/components/common/v2/dropdown/SortingDropdown.tsx
+++ src/components/common/v2/dropdown/SortingDropdown.tsx
@@ -1,14 +1,29 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import Button from '@/components/common/v2/button/Button';
 import SORT_BY from '@/constants/sortType';
 
-function SortingDropdown() {
+type SortOrderType = keyof typeof SORT_BY;
+interface SortingDropDownType {
+	// sortOrder: SortOrderType;
+	handleSortOrder: (order: SortOrderType) => void;
+}
+function SortingDropdown({ handleSortOrder }: SortingDropDownType) {
+	// TODO: 선택된 스타일 추가 설정해야 할 듯
 	return (
 		<SortingDropdownContainer>
-			{Object.values(SORT_BY).map((label, index) => (
+			{Object.keys(SORT_BY).map((label, index) => (
 				<ButtonLayout key={label}>
-					<Button label={label} onClick={() => {}} size="large" type="text-assistive" />
+					<Button
+						label={SORT_BY[label as SortOrderType]}
+						onClick={() => {
+							handleSortOrder(label as SortOrderType);
+						}}
+						size="large"
+						type="text-assistive"
+						additionalCss={btnCustomWidth}
+					/>
 					{(index === 0 || index === 2) && <Divider />}
 				</ButtonLayout>
 			))}
@@ -16,7 +31,12 @@ function SortingDropdown() {
 	);
 }
 
+const btnCustomWidth = css`
+	width: 100%;
+`;
 const SortingDropdownContainer = styled.div`
+	position: absolute;
+	z-index: 4;
 	display: flex;
 	flex-direction: column;
 	box-sizing: border-box;
@@ -24,6 +44,7 @@ const SortingDropdownContainer = styled.div`
 	height: 28.8rem;
 	padding: 0.8rem;
 
+	background-color: ${({ theme }) => theme.color.Grey.White};
 	border-radius: 12px;
 	${({ theme }) => theme.shadow.FloatingAction3};
 `;
