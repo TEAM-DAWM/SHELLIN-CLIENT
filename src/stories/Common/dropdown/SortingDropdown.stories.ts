a857862ff10c266c478b0e51a955543a79a90ee0
wrryu09 <rryu09@ewhain.net>
2025-01-12 22:30:14 +0900

    docs: fix sortingdropdown storybook

--- src/stories/Common/dropdown/SortingDropdown.stories.ts
+++ src/stories/Common/dropdown/SortingDropdown.stories.ts
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import SortingDropdown from '@/components/common/v2/dropdown/SortingDropdown';
@@ -15,4 +16,8 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+	args: {
+		handleSortOrder: action('handleSortOrder'), // 버튼 클릭 시 액션 로그 출력
+	},
+};
