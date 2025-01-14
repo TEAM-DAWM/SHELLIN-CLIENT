41501adbf2c139071897389bc0dae36c23130254
Kjiw0n <rwd0904@naver.com>
2025-01-14 23:22:53 +0900

    refactor: google calendar 내용 주석처리

--- src/apis/timeBlocks/getTimeBlock/query.ts
+++ src/apis/timeBlocks/getTimeBlock/query.ts
@@ -4,10 +4,10 @@ import getTimeBlock from './axios';
 import { GetTimeBlokType } from './GetTimeBlockType';
 
 /** TimeBlock 리스트 조회 */
-const useGetTimeBlock = ({ startDate, range, categories }: GetTimeBlokType) =>
+const useGetTimeBlock = ({ startDate, range }: GetTimeBlokType) =>
 	useQuery({
-		queryKey: ['timeblock', startDate, range, categories],
-		queryFn: () => getTimeBlock({ startDate, range, categories }),
+		queryKey: ['timeblock', startDate, range],
+		queryFn: () => getTimeBlock({ startDate, range }),
 	});
 
 export default useGetTimeBlock;
