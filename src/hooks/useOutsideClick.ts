b363336f873babcb0927f1360eda6d5995f762f0
Kjiw0n <rwd0904@naver.com>
2025-01-21 00:20:54 +0900

    style: 외부영역 배경색 제거

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -8,26 +8,14 @@ const useOutsideClick = <T extends HTMLElement>({ onClose }: OutsideClickHandler
 	const ref = useRef<T>(null);
 
 	useEffect(() => {
-		const overlay = document.createElement('div');
-		overlay.style.position = 'fixed';
-		overlay.style.top = '0';
-		overlay.style.left = '0';
-		overlay.style.width = '100%';
-		overlay.style.height = '100%';
-		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-		overlay.style.zIndex = '3';
-		document.body.appendChild(overlay);
-
 		const handleClickOutside = (event: MouseEvent) => {
 			if (ref.current && !ref.current.contains(event.target as Node)) {
-				onClose(); // 외부 클릭 시 모달 닫기
-				document.body.removeChild(overlay); // 배경 제거
+				onClose();
 			}
 		};
 
 		document.addEventListener('mousedown', handleClickOutside);
 
-		// 컴포넌트 언마운트 시 배경 및 이벤트 제거
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside);
 		};
