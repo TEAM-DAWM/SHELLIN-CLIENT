3e01dbdadf663d7392cf6a590dc2fa2a34eef5a1
wrryu09 <rryu09@ewhain.net>
2025-01-22 00:47:41 +0900

    fix: 반환타입지정

--- src/apis/tasks/taskDescription/axios.ts
+++ src/apis/tasks/taskDescription/axios.ts
@@ -1,14 +1,15 @@
 import { TaskDescriptionType } from './TaskDescriptionType';
 
 import { privateInstance } from '@/apis/instance';
+import { DetailedTaskType } from '@/types/tasks/taskType';
 
-const taskDescription = async ({ taskId, targetDate }: TaskDescriptionType) => {
+const taskDescription = async ({ taskId, targetDate }: TaskDescriptionType): Promise<DetailedTaskType> => {
 	const { data } = await privateInstance.get(`/api/tasks/${taskId}`, {
 		params: {
 			targetDate,
 		},
 	});
-	return data;
+	return data.data;
 };
 
 export default taskDescription;
