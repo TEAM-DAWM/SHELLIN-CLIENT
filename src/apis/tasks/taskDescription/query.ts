066adda70764cd2a6376e257cf2b62cfd06ea285
wrryu09 <rryu09@ewhain.net>
2025-01-21 23:54:50 +0900

    fix: useTaskDescription isopen 추가, 쿼리키 today 제거

--- src/apis/tasks/taskDescription/query.ts
+++ src/apis/tasks/taskDescription/query.ts
@@ -4,10 +4,11 @@ import taskDescription from './axios';
 import { TaskDescriptionType } from './TaskDescriptionType';
 
 // Task 상세 설명 조회
-const useTaskDescription = ({ taskId, targetDate }: TaskDescriptionType) => {
+const useTaskDescription = ({ taskId, targetDate, isOpen }: TaskDescriptionType) => {
 	const data = useQuery({
-		queryKey: ['today', 'taskDesc', taskId, targetDate],
+		queryKey: ['taskDesc', taskId, targetDate],
 		queryFn: () => taskDescription({ taskId, targetDate }),
+		enabled: isOpen,
 	});
 
 	return data;
