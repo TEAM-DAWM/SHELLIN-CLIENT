f1559e9ec5cf483fadcf0e704893a8e215e20d15
JeeminYi <yjjmmm000@g.skku.edu>
2025-01-25 10:56:33 +0900

    style: 위치 조정

--- src/components/common/EmptyViewStaging.tsx
+++ src/components/common/EmptyViewStaging.tsx
@@ -13,10 +13,13 @@ function EmptyViewStaging() {
 export default EmptyViewStaging;
 
 const EmptyView = styled.div`
+	position: absolute;
+	inset: -0.8rem 10.4rem 34rem;
 	display: flex;
+	align-items: center;
+	justify-content: center;
 	width: 24rem;
 	height: 52rem;
-	margin-top: 4rem;
 `;
 
 const EmptyImg = styled.img`
