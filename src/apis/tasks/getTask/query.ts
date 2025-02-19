9aebaba87adb627bfd611aeb0bd2f3a6e1630b46
moonseonghui <msh9980@naver.com>
2025-02-19 18:06:46 +0900

    fix: task 리스트 조회 시 targeDate 유무로 today, staging 나눠서 쿼리키 설정하도록 수정

--- src/apis/tasks/getTask/query.ts
+++ src/apis/tasks/getTask/query.ts
@@ -3,12 +3,14 @@ import { useQuery } from '@tanstack/react-query';
 import getTasks from './axios';
 import { GetTasksType } from './GetTasksType';
 
+import QUERY_KEYS from '@/apis/queryKeys';
+
 /** Task 리스트 조회 */
 const useGetTasks = ({ sortOrder, targetDate }: GetTasksType) =>
 	useQuery({
-		queryKey: ['today', sortOrder, targetDate],
+		// taskgetDate가 유무로 today, staging 쿼리키 설정
+		queryKey: [...(targetDate ? QUERY_KEYS.today() : QUERY_KEYS.staging()), sortOrder, targetDate],
 		queryFn: () => getTasks({ sortOrder, targetDate }),
-		placeholderData: [],
 	});
 
 export default useGetTasks;
