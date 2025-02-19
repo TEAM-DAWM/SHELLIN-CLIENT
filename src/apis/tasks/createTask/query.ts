da79aa666f14a6d17d8b82dc83830b28a26bc94c
moonseonghui <msh9980@naver.com>
2025-02-19 18:05:07 +0900

    fix: task추가시 statging 쿼리키만 무효화 되도록 수정

--- src/apis/tasks/createTask/query.ts
+++ src/apis/tasks/createTask/query.ts
@@ -2,13 +2,15 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import createTask from './axios';
 import { CreateTaskType } from './CreateTaskType';
+
+import QUERY_KEYS from '@/apis/queryKeys';
 /** Task 추가 */
 const useCreateTask = () => {
 	const queryClient = useQueryClient();
 
 	const mutation = useMutation({
 		mutationFn: ({ name, deadLine }: CreateTaskType) => createTask({ name, deadLine }),
-		onSuccess: () => queryClient.invalidateQueries({ queryKey: ['today'] }),
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: QUERY_KEYS.staging() }),
 	});
 
 	return { mutate: mutation.mutate };
