4ab2fd2cf38b104032ab6d14a7800e363e2825c7
moonseonghui <msh9980@naver.com>
2025-02-19 18:13:28 +0900

    fix: 수정된 쿼리 적용

--- src/apis/tasks/taskDescription/query.ts
+++ src/apis/tasks/taskDescription/query.ts
@@ -3,10 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 import taskDescription from './axios';
 import { TaskDescriptionType } from './TaskDescriptionType';
 
+import QUERY_KEYS from '@/apis/queryKeys';
+
 // Task 상세 설명 조회
 const useTaskDescription = ({ taskId, targetDate, isOpen }: TaskDescriptionType) => {
 	const data = useQuery({
-		queryKey: ['taskDesc', taskId, targetDate],
+		queryKey: QUERY_KEYS.taskDescription(taskId, targetDate),
 		queryFn: () => taskDescription({ taskId, targetDate }),
 		enabled: isOpen,
 	});
