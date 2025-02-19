332cba5b4b54b6ca1ddc1ee386cfa6568963f8aa
moonseonghui <msh9980@naver.com>
2025-02-19 18:12:54 +0900

    fix: task 상태 수정 쿼리키 무효화 병렬로 실행하도록 수정

--- src/apis/tasks/updateTaskStatus/query.ts
+++ src/apis/tasks/updateTaskStatus/query.ts
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import updateTaskStatus from './axios';
 import { UpdateTaskStatusType } from './UpdateTaskStatusType';
 
+import QUERY_KEYS from '@/apis/queryKeys';
 import { useToast } from '@/components/toast/ToastContext';
 
 const useUpdateTaskStatus = (handleIconMouseLeave: (() => void) | null) => {
@@ -11,19 +12,23 @@ const useUpdateTaskStatus = (handleIconMouseLeave: (() => void) | null) => {
 
 	const mutation = useMutation({
 		mutationFn: (updateData: UpdateTaskStatusType) => updateTaskStatus(updateData),
-		onSuccess: (_, updateData) => {
+		onSuccess: async (_, updateData) => {
 			addToast('변경사항이 적용되었어요', 'success');
-			queryClient.invalidateQueries({ queryKey: ['today'] }).then(() => {
-				if (handleIconMouseLeave) {
-					handleIconMouseLeave();
-				}
-			});
-			if (updateData.status === '미완료') {
-				queryClient.invalidateQueries({ queryKey: ['dashboard'] });
-			}
-			if (updateData.status === '진행 중' || updateData.status === '완료') {
-				queryClient.invalidateQueries({ queryKey: ['dashboard', 'inprogress'] });
-			}
+
+			// staging과 today중 해당 task가 속한 영역만 쿼리키 무효화
+			const cachedTask = queryClient.getQueryData(QUERY_KEYS.taskDescription(updateData.taskId));
+			const listQueryKey = cachedTask ? QUERY_KEYS.staging() : QUERY_KEYS.today();
+
+			await Promise.all([
+				queryClient.invalidateQueries({ queryKey: listQueryKey }),
+
+				updateData.status === '미완료' ? queryClient.invalidateQueries({ queryKey: QUERY_KEYS.dashboard() }) : null,
+				updateData.status === '진행중' || updateData.status === '완료'
+					? queryClient.invalidateQueries({ queryKey: QUERY_KEYS.dashboardInProgress() })
+					: null,
+			]);
+
+			if (handleIconMouseLeave) handleIconMouseLeave();
 		},
 	});
 
