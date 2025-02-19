9262896f84a3043b207e1413fa5edb3002f998b9
moonseonghui <msh9980@naver.com>
2025-02-19 18:28:52 +0900

    fix: staging area의 경우 Todo 컴포넌트로 targetDate 넘겨주지 않도록 수정

--- src/components/common/StagingArea/StagingAreaTaskContainer.tsx
+++ src/components/common/StagingArea/StagingAreaTaskContainer.tsx
@@ -14,15 +14,10 @@ interface StagingAreaTaskContainerProps {
 	handleSelectedTarget: (task: TaskType | null) => void;
 	selectedTarget: TaskType | null;
 	tasks: TaskType[];
-	targetDate: string;
+	targetDate?: string;
 }
 
-function StagingAreaTaskContainer({
-	handleSelectedTarget,
-	selectedTarget,
-	tasks,
-	targetDate,
-}: StagingAreaTaskContainerProps) {
+function StagingAreaTaskContainer({ handleSelectedTarget, selectedTarget, tasks }: StagingAreaTaskContainerProps) {
 	useEffect(() => {
 		const container = document.getElementById('dumping-task-container');
 
@@ -89,7 +84,7 @@ function StagingAreaTaskContainer({
 												deadlineTime={task.deadLine?.time || undefined}
 												isStatusVisible={false}
 												taskId={task.id}
-												targetDate={targetDate}
+												targetDate=""
 												onClick={() => handleSelectedTarget(task)}
 											/>
 										</div>
