07045bb3535476d73b0607e0f1f1fd1e51ffb8e5
moonseonghui <msh9980@naver.com>
2025-02-19 18:05:47 +0900

    fix: task 삭제시 삭제하는 task가 속한 영역만 쿼리키 무효화되도록 수정

--- src/apis/tasks/deleteTask/query.ts
+++ src/apis/tasks/deleteTask/query.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import deleteTask from './axios';
 
+import QUERY_KEYS from '@/apis/queryKeys';
 import { useToast } from '@/components/toast/ToastContext';
 
 /** Task 삭제 */
@@ -11,9 +12,14 @@ const useDeleteTask = () => {
 
 	const mutation = useMutation({
 		mutationFn: (taskId: number) => deleteTask(taskId),
-		onSuccess: () => {
+		onSuccess: (_, taskId) => {
 			addToast('할 일이 삭제되었어요', 'error');
-			queryClient.invalidateQueries({ queryKey: ['today'] });
+
+			// staging과 today중 해당 task가 속한 영역만 쿼리키 무효화
+			const cachedTask = queryClient.getQueryData(QUERY_KEYS.taskDescription(taskId));
+			const listQueryKey = cachedTask ? QUERY_KEYS.staging() : QUERY_KEYS.today();
+
+			queryClient.invalidateQueries({ queryKey: listQueryKey });
 			queryClient.invalidateQueries({ queryKey: ['timeblock'] });
 		},
 	});
