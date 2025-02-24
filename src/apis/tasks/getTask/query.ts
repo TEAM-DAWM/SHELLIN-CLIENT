3d2e9dd38a5a6033292f4105259ef10cd1904b30
moonseonghui <msh9980@naver.com>
2025-02-24 17:57:54 +0900

    refactor: QUERY_KEYS.today와 QUERY_KEYS.staging의 쿼리 키 생성 로직 개선
    - 불필요한 빈 문자열() 제거하여 불필요한 키 추가 방지
    - sortOrder가 존재할 때만 추가되도록 수정

--- src/apis/tasks/getTask/query.ts
+++ src/apis/tasks/getTask/query.ts
@@ -6,11 +6,14 @@ import { GetTasksType } from './GetTasksType';
 import QUERY_KEYS from '@/apis/queryKeys';
 
 /** Task 리스트 조회 */
-const useGetTasks = ({ sortOrder, targetDate }: GetTasksType) =>
-	useQuery({
-		// taskgetDate가 유무로 today, staging 쿼리키 설정
-		queryKey: [...(targetDate ? QUERY_KEYS.today() : QUERY_KEYS.staging()), sortOrder, targetDate],
+const useGetTasks = ({ sortOrder, targetDate }: GetTasksType) => {
+	// taskgetDate가 유무로 today, staging 쿼리키 설정
+	const baseQueryKey = targetDate ? QUERY_KEYS.today() : QUERY_KEYS.staging();
+
+	return useQuery({
+		queryKey: [...baseQueryKey, sortOrder, targetDate],
 		queryFn: () => getTasks({ sortOrder, targetDate }),
 	});
+};
 
 export default useGetTasks;
