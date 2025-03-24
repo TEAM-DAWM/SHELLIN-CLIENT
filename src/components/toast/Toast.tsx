ffbef764a4059e2be12acb5bf4a9d8c81dddf137
wrryu09 <rryu09@ewhain.net>
2025-03-25 00:31:57 +0900

    feat: toast 에 기능 연결

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -38,8 +38,8 @@ function Toast({ message, onClose, code, onRevert }: ToastProps) {
 				{message}
 			</TextLayout>
 			<RevertBox>
-				<RevertText onClick={onRevert}>되돌리기</RevertText>
-				<Icon name="IcnX" />
+				{onRevert && <RevertText onClick={onRevert}>되돌리기</RevertText>}
+				<Icon name="IcnX" onClick={onClose} />
 			</RevertBox>
 		</ToastMessage>
 	);
