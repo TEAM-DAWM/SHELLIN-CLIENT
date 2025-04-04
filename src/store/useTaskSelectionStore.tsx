e9007463bc4e1f151303f2b39c7117172dc8ccea
Kjiw0n <rwd0904@naver.com>
2025-04-04 16:59:47 +0900

    feat: add method to clear selectedTask

--- src/store/useTaskSelectionStore.tsx
+++ src/store/useTaskSelectionStore.tsx
@@ -5,11 +5,13 @@ import { TaskType } from '@/types/tasks/taskType';
 interface TaskSelectionState {
 	selectedTask: TaskType | null;
 	setSelectedTask: (task: TaskType) => void;
+	clearSelectedTask: () => void;
 }
 
 const useTaskSelectionStore = create<TaskSelectionState>((set) => ({
 	selectedTask: null,
 	setSelectedTask: (task: TaskType) => set({ selectedTask: task }),
+	clearSelectedTask: () => set({ selectedTask: null }),
 }));
 
 export default useTaskSelectionStore;
