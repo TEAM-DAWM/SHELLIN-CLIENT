1df947e0fa9cf554861eb2e079e5be323748c297
Kjiw0n <rwd0904@naver.com>
2025-04-06 14:56:33 +0900

    feat: add isDragging state in useTaskSelectionStore

--- src/store/useTaskSelectionStore.tsx
+++ src/store/useTaskSelectionStore.tsx
@@ -6,12 +6,16 @@ interface TaskSelectionState {
 	selectedTask: TaskType | null;
 	setSelectedTask: (task: TaskType) => void;
 	clearSelectedTask: () => void;
+	isDragging: boolean;
+	setIsDragging: (dragging: boolean) => void;
 }
 
 const useTaskSelectionStore = create<TaskSelectionState>((set) => ({
 	selectedTask: null,
+	isDragging: false,
 	setSelectedTask: (task: TaskType) => set({ selectedTask: task }),
 	clearSelectedTask: () => set({ selectedTask: null }),
+	setIsDragging: (dragging: boolean) => set({ isDragging: dragging }),
 }));
 
 export default useTaskSelectionStore;
